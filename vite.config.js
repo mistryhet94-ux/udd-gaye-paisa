f213b47b182259bc7d27db2741991be176715ca8
mistryhet94-ux <het@uddgayepaisa.com>
2026-07-01 05:06:11 +0000

    Add PWA support - installable on mobile

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Udd Gaye Paisa',
+        short_name: 'UddGayePaisa',
+        description: 'Personal finance tracker',
+        theme_color: '#0b0d10',
+        background_color: '#0b0d10',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
