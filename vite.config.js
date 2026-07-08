11b1c118aff7dcef492ec6dadc0feccb6a900b30
mistryhet94-ux <het@uddgayepaisa.com>
2026-07-08 02:45:11 +0000

    Force cache bust with v3 filename suffix

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v3.js`,
+        chunkFileNames: `assets/[name]-[hash]-v3.js`,
+      }
+    }
+  }
 })
